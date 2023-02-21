69f05ee12cb6e5e0e05d2da3bb948f7c6c0c0796
Tebrawy <boodytebry@gmail.com>
2023-02-21 14:46:26 +0200

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int j = 0;
+
+	while (j < 10)
+	{
+		j++;
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+	}
+	_putchar('\n');
+}
