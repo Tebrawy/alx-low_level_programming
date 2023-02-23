c968cd9ee53d9e009ac36a6a65aecd047d9a5797
Tebrawy <boodytebry@gmail.com>
2023-02-23 16:09:25 +0200

    More nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,16 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
-	_putchar('\n');
-	} else
+		_putchar('\n');
+	} 
+	else
 	{
-	int i;
+		int i;
 
 	for (i = 1; i <= n; i++)
 	{
-	_putchar('_');
+		_putchar('_');
 	}
-	_putchar('\n');
+		_putchar('\n');
 	}
-
 }
