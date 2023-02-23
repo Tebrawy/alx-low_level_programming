491abd2e66c97d94c9c97cf41804aedcf454d85b
Tebrawy <boodytebry@gmail.com>
2023-02-23 15:31:09 +0200

    More nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiply
+ * @a: is the first integar
+ * @b: is the second integar
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
