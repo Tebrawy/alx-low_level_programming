baca81b6791171c6ab8a1972cef825eed04a9480
Tebrawy <boodytebry@gmail.com>
2023-02-27 15:15:02 +0200

    C - pointers and arrrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps int a with b
+ * @a: integar to swap
+ * @b: integar to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	m = *a;
+	*a = *b;
+	*b = m;
+}
